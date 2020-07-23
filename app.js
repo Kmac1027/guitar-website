99b147a8146152052afc85e73a5f623db9a95b27
Kevin McNeil <dogwoodrawks@gmail.com>
2020-07-23 11:47:01 -0700

    added functions to javascript

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var like = prompt('Would you Like to Learn to Play Guitar?');
-
-if (like === 'yes'){
-    alert('WELCOME! This is the right place for you!');
-} else if (like === 'no'){
-    alert('Maybe this isnt the Website for you')
+function likeGuitar(){
+    var like = prompt('Would you Like to Learn to Play Guitar?');
+    if (like === 'yes'){
+        alert('WELCOME! This is the right place for you!');
+    } 
+    else if (like === 'no'){
+        alert('Maybe this isnt the Website for you')
+    } 
+    else{
+        alert('Sorry i didnt get that?')
+    }
 }
+likeGuitar();
+
 var userTime = prompt('what time is it?');
 
 if (userTime < 12 ){
@@ -15,6 +22,14 @@ if (userTime < 12 ){
     document.write('<h3>' + 'Good Evening' + '</h3>')
 }
 
-var userName = prompt('what is your name?');
+function getuserName(){
+    var userNameInFunction = prompt('what is your name?');
+    //document.write('<h3>' + userNameInFunction + '</h3>');
+    return userNameInFunction;
+}
+
+var userName = getuserName();
 
 document.write('<h3>' + userName + '</h3>');
+
+
