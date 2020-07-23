387e7a9fa7649429fb77aa99c644ea14d28da48e
Kevin McNeil <dogwoodrawks@gmail.com>
2020-07-23 15:54:23 -0700

    added a loop in Javascript

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function likeGuitar(){
     var like = prompt('Would you Like to Learn to Play Guitar?');
     if (like === 'yes'){
@@ -9,8 +10,29 @@ function likeGuitar(){
     else{
         alert('Sorry i didnt get that?')
     }
+    return like;
 }
-likeGuitar();
+// likeGuitar();
+
+// var like;
+// while(true){ 
+//     like = prompt('Would you Like to Learn to Play Guitar?');
+//     if(like == 'yes'){
+//         alert('welcome');
+//         break;
+//     }
+//     if(like == 'no'){
+//         alert('Maybe This isnt the Website for You');
+//         break;
+//     }
+//     alert('Sorry, I didnt get that, Please answer yes or no');
+// }
+
+var blah;
+while(blah != 'yes' && blah != 'no'){
+    blah = likeGuitar()
+}
+
 
 var userTime = prompt('what time is it?');
 
@@ -32,4 +54,27 @@ var userName = getuserName();
 
 document.write('<h3>' + userName + '</h3>');
 
+// for (var i = 0; i <= 12; i = i + 1) {
+//    console.log(1 * 8);
+// }
 
+var wannaPlay = prompt('Do you want to play a game');
+var correctAnswer = 12;
+
+while(wannaPlay == 'yes' || wannaPlay == 'Yes'){
+    for (var i = 0; i < 100; i = i + 1){
+        var userAnswer = prompt('please enter a number between 1 and 20');
+        if(userAnswer == correctAnswer){
+            wannaPlay = prompt('DING DING DING, THAT IS CORRECT!!! would you like to play again?');
+            break;
+        } else {
+            if (userAnswer < correctAnswer){
+            alert('Incorrect, too low, try again');
+        } else {
+            if (userAnswer > correctAnswer){
+            alert('Incorrect, too high, try again');
+            }
+        }
+    }
+  }
+}
